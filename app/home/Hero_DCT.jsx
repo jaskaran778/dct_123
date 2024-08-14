49309c46300897a1e0bb0fb4e3bce96e1c2d7d15
SANS-1104 <sanskriti020803@gmail.com>
2024-08-14 15:14:43 +0530

    commit 2

--- app/home/Hero_DCT.jsx
+++ app/home/Hero_DCT.jsx
@@ -8,9 +8,9 @@ export default function Hero() {
     <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-[40rem] overflow-hidden mb-28 mq750:mt-[-10vh]">
       <Vortex
         backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-16 w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+        <h2 className="text-white text-2xl md:text-5xl font-bold text-center">
           Pioneering Innovation <br /> with Smart Technology Solutions
         </h2>
         {/* <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
