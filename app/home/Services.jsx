b4984fa644cb6410b25d50e244e8df6430eed85e
Jaskaran Singh <jaskarans9650@gmail.com>
2024-08-17 13:55:28 +0530

    footer services link

--- app/home/Services.jsx
+++ app/home/Services.jsx
@@ -31,7 +31,9 @@ export default function ServicesPage() {
   return (
     <div className="services-section flex flex-col items-center justify-center mt-20 text-white">
       <h1 className="text-[10vh] font-bold mq750:text-[5vh]">DCT SERVICES</h1>
-      <ServiceContent />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ServiceContent />
+      </Suspense>
     </div>
   );
 }
